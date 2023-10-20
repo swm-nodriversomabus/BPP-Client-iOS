86d6fcc13446116581d7d31d7f43054502c5c5b3
JinHo Shin <dpdlalslr@gmail.com>
2023-10-20 18:40:30 +0900

    Initial Commit

---
+++ triparty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  triparty
+//
+//  Created by 신진호 on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ triparty/tripartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripartyApp.swift
+//  triparty
+//
+//  Created by 신진호 on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct tripartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
