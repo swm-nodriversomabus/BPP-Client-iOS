543c822e3ce727c4205a623a38c6d06e0828e7d7
JinHo Shin <dpdlalslr@gmail.com>
2023-10-20 19:25:42 +0900

    feat(iOS): 웹뷰에 페이지 띄움

--- triparty/ContentView.swift
+++ triparty/ContentView.swift
@@ -6,18 +6,28 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(url: URL(string: "https://dev.yeohaengparty.com/")!)
     }
 }
+
+
+struct WebView: UIViewRepresentable {
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: url))
+    }
+    var url: URL
+    func makeUIView(context: Context) -> WKWebView {
+        let webConfig = WKWebViewConfiguration()
+        webConfig.defaultWebpagePreferences.allowsContentJavaScript = true
+        webConfig.allowsInlineMediaPlayback = true
+        return WKWebView(frame: CGRect(x: 0, y: 0, width: 100, height: 100), configuration: webConfig)
+    }
+}
+
 
 #Preview {
     ContentView()
